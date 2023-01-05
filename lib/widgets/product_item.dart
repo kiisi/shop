d0e1fc1714dd2e9d05eb8fcd632e954efb265466
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-06 00:40:26 +0100

    Adding Hero Transition

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -18,10 +18,13 @@ class ProductItem extends StatelessWidget {
       ),
       child: GridTile(
         child: GestureDetector(
-          child: FadeInImage(
-            placeholder: AssetImage("assets/images/product-placeholder.png"),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage("assets/images/product-placeholder.png"),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
           onTap: () {
             Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
